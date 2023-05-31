980eb892607fe507db084e75f1040e66ccf520d6
Ande Bell <ande.bell@dymaptic.com>
2023-05-31 14:59:25 -0600

    project checkin

---
+++ WpfApp1/test1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+
+namespace WpfBlazor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class test1 : UserControl
+    {
+        public test1()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
